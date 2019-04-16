64946fed3c7d1e2530a4c0e4803d4a526f865b4d
Matt Gill <matthew.gill@live.co.uk>
2019-04-16 23:16:11 +0100

    Completed tilemap implementation.

--- core/src/uk/ac/aston/dc2060/TowerDefenceGame.java
+++ core/src/uk/ac/aston/dc2060/TowerDefenceGame.java
@@ -2,48 +2,53 @@ package uk.ac.aston.dc2060;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.MapRenderer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class TowerDefenceGame extends ApplicationAdapter {
 
-    // Sprite components
-    private SpriteBatch batch;
-    
     // Map components
-    private OrthographicCamera mapCamera;
     private MapRenderer mapRenderer;
     private TiledMap map;
 
+    // Sprite components
+    private SpriteBatch batch;
+
     @Override
     public void create () {
-        batch = new SpriteBatch();
+        // Configure Map rendering
         map = new TmxMapLoader().load("tilemap.tmx");
         mapRenderer = new OrthogonalTiledMapRenderer(map, 50 / 64f);
-        mapCamera = new OrthographicCamera(800, 600);
-        mapCamera.setToOrtho(false);
+        OrthographicCamera mapCamera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        mapCamera.setToOrtho(true);
+        mapCamera.update();
+        mapRenderer.setView(mapCamera);
+
+        // Configure sprite rendering
+        batch = new SpriteBatch();
     }
 
     @Override
     public void render () {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        mapCamera.update();
+
+        // Render map
         mapRenderer.render();
+
+        // Render everything else
+        batch.begin();
         batch.end();
     }
 
     @Override
     public void dispose () {
-        batch.dispose();
         map.dispose();
+        batch.dispose();
     }
 }
