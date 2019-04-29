af59561b02267965bb146806fc1f976ec2fddd7f
Matt Gill <matthew.gill@live.co.uk>
2019-04-29 09:22:09 +0100

    Updated window name.

--- desktop/src/uk/ac/aston/dc2060/desktop/DesktopLauncher.java
+++ desktop/src/uk/ac/aston/dc2060/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import uk.ac.aston.dc2060.TowerDefenceGame;
 public class DesktopLauncher {
     public static void main (String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.title = "TowerIcon Defence Game";
+        config.title = "Tower Defence Game";
         config.width = 1200;
         config.height = 900;
         new LwjglApplication(new TowerDefenceGame(), config);
