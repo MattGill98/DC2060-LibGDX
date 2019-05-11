75100701225087a46b0865aef8adb2d24500d97e
Matt Gill <matthew.gill@live.co.uk>
2019-05-11 17:04:09 +0100

    Added number object.

--- core/src/uk/ac/aston/dc2060/model/TileID.java
+++ core/src/uk/ac/aston/dc2060/model/TileID.java
@@ -9,7 +9,17 @@ public enum TileID {
     SINGLE_TURRET_BASE(180),
     SINGLE_TURRET_GUNFIRE(296),
     DOUBLE_TURRET(250),
-    SOLDIER(245);
+    SOLDIER(245),
+    ZERO(276),
+    ONE(277),
+    TWO(278),
+    THREE(279),
+    FOUR(280),
+    FIVE(281),
+    SIX(282),
+    SEVEN(283),
+    EIGHT(284),
+    NINE(285);
 
     private int id;
 
