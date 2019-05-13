904dea1173616cd7e19bf90513baaa65dd995155
Matt Gill <matthew.gill@live.co.uk>
2019-05-13 16:38:23 +0100

    Removed second buffer clear.

--- core/src/uk/ac/aston/dc2060/TowerDefenceGame.java
+++ core/src/uk/ac/aston/dc2060/TowerDefenceGame.java
@@ -3,7 +3,6 @@ package uk.ac.aston.dc2060;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
@@ -42,10 +41,6 @@ public class TowerDefenceGame extends ApplicationAdapter {
 
     @Override
     public void render () {
-        // Clear the window
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
         float delta = Gdx.graphics.getDeltaTime();
         welcomeScreen.render(delta);
         towerDefenceScreen.render(delta);
